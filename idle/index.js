9be7e930ace25350d0bf13159b63a3fa198431e4
Lawrence Zhou <60532041+illonion@users.noreply.github.com>
2025-07-11 17:48:38 +1000

    Add song title and name

--- idle/index.js
+++ idle/index.js
@@ -3,3 +3,27 @@ const roundNameEl = document.getElementById("round-name")
 const matchTypeEl = document.getElementById("match-type")
 const currentStreamEl = document.getElementById("current-stream")
 const setIdleTitle = () => currentStreamEl.setAttribute("src", `static/idle-icons/${matchTypeEl.value}-${roundNameEl.value}.png`)
+
+// Now Playing Information
+const songNameEl = document.getElementById("song-name")
+const songArtistEl = document.getElementById("song-artist")
+let currentId, currentChecksum
+// Song Timeline Information
+const songTimelineForegroundEl = document.getElementById("song-timeline-foreground")
+const songTimelineCircleEl = document.getElementById("song-timeline-circle")
+const songCurrentTimeEl = document.getElementById("song-current-time")
+const songEndTimeEl = document.getElementById("song-end-time")
+
+// Socket
+const socket = createTosuWsSocket()
+socket.onmessage = event => {
+    const data = JSON.parse(event.data)
+    console.log(data)
+
+    if (currentId !== data.beatmap.id && currentChecksum !== data.beatmap.checksum) {
+        currentId = data.beatmap.id
+        currentChecksum = data.beatmap.checksum
+        songNameEl.textContent = data.beatmap.title
+        songArtistEl.textContent = data.beatmap.artist
+    }
+}
