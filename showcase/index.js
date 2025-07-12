5d350acb8f6a0065e10b53ef99bfe585c8f90df0
Lawrence Zhou <60532041+illonion@users.noreply.github.com>
2025-07-12 16:24:29 +1000

    Reconfigure index.js to work properly

--- showcase/index.js
+++ showcase/index.js
@@ -6,7 +6,6 @@ async function getBeatmaps() {
 getBeatmaps()
 const findBeatmaps = beatmap_string => allBeatmaps.find(beatmap => beatmap.beatmap_string === beatmap_string)
 
-
 // Gameplay information
 const replayerNameEl = document.getElementById("replayer-name")
 const mapperNameEl = document.getElementById("mapper-name")
@@ -14,7 +13,7 @@ const mapperNameEl = document.getElementById("mapper-name")
 const songNameEl = document.getElementById("song-name")
 const songArtistEl = document.getElementById("song-artist")
 const songDifficultyEl = document.getElementById("song-difficulty")
-let currentId
+let currentId, currentChecksum
 
 // Stats
 const bpmMumberEl = document.getElementById("bpm-number")
@@ -38,57 +37,65 @@ socket.onmessage = event => {
     const data = JSON.parse(event.data)
     console.log(data)
 
-    const currentMap = findBeatmaps(`${data.beatmap.artist} - ${data.beatmap.title} [${data.beatmap.version}]`)
-    if (currentMap) {
-        modIconEl.style.display = "block"
-        modIconEl.setAttribute("src", `static/mods/${currentMap.mod}${currentMap.order}.png`)
-    } else {
-        modIconEl.style.display = "none"
+    // Set mod id based on map
+    if (currentId !== data.beatmap.id || currentChecksum !== data.beatmap.checksum) {
+        currentId = data.beatmap.id
+        currentChecksum = data.beatmap.checksum
+        const currentMap = findBeatmaps(`${data.beatmap.artist} - ${data.beatmap.title} [${data.beatmap.version}]`)
+        if (currentMap) {
+            modIconEl.style.display = "block"
+            modIconEl.setAttribute("src", `static/mods/${currentMap.mod}${currentMap.order}.png`)
+        } else {
+            modIconEl.style.display = "none"
+        }
+
+        // Set metadata details
+        songNameEl.textContent = data.beatmap.title
+        songArtistEl.textContent = data.beatmap.artist
+        songDifficultyEl.textContent = data.beatmap.version
+        mapperNameEl.textContent = data.beatmap.mapper
+
+        // Set a few timing details
+        const backgroundUrl = data.directPath.beatmapBackground.replace(/\\/g, "/")
+        const imagePath = `../../Songs/${backgroundUrl}?a=${Math.random(10000)}`
+        songNameEl.textContent = data.beatmap.title
+        songArtistEl.textContent = data.beatmap.artist
+
+        // Get dominant colour
+        const img = new Image()
+        img.crossOrigin = "anonymous"
+        img.src = imagePath
+
+        img.onload = function () {
+            // Get base colour
+            const colorThief = new ColorThief()
+            const baseColor = colorThief.getColor(img)
+
+            // Get scaled colour based on baseColor
+            let scaledColor = baseColor
+            let multiplier = 1
+            while (scaledColor.reduce((a, b) => a + b, 0) < 500) {
+                multiplier += 0.1
+                scaledColor = baseColor.map(c => c * multiplier)
+            }
+            const borderColor = scaledColor.map(c => Math.round(c * 0.8))
+
+            // Set backgrounds / borders
+            songTimelineForegroundEl.style.backgroundColor = `rgb(${scaledColor.join(",")})`
+            songTimelineCircleEl.style.backgroundColor = `rgb(${scaledColor.join(",")})`
+            songTimelineCircleEl.style.borderColor = `rgb(${borderColor.join(",")})`
+        }
+
     }
 
     replayerNameEl.textContent = data.resultsScreen.playerName
-    mapperNameEl.textContent = data.beatmap.mapper
-
-    songNameEl.textContent = data.beatmap.title
-    songArtistEl.textContent = data.beatmap.artist
-    songDifficultyEl.textContent = data.beatmap.version
-
+    
+    // Set stat numbers
     bpmMumberEl.textContent = data.beatmap.stats.bpm.common
     csMumberEl.textContent = data.beatmap.stats.cs.converted
     arMumberEl.textContent = data.beatmap.stats.ar.converted
     odMumberEl.textContent = data.beatmap.stats.od.converted
     srMumberEl.textContent = `${data.beatmap.stats.stars.total}*`
-
-
-    const backgroundUrl = data.directPath.beatmapBackground.replace(/\\/g, "/")
-    const imagePath = `../../Songs/${backgroundUrl}?a=${Math.random(10000)}`
-    songNameEl.textContent = data.beatmap.title
-    songArtistEl.textContent = data.beatmap.artist
-
-    // Get dominant colour
-    const img = new Image()
-    img.crossOrigin = "anonymous"
-    img.src = imagePath
-
-    img.onload = function () {
-        // Get base colour
-        const colorThief = new ColorThief()
-        const baseColor = colorThief.getColor(img)
-
-        // Get scaled colour based on baseColor
-        let scaledColor = baseColor
-        let multiplier = 1
-        while (scaledColor.reduce((a, b) => a + b, 0) < 500) {
-            multiplier += 0.1
-            scaledColor = baseColor.map(c => c * multiplier)
-        }
-        const borderColor = scaledColor.map(c => Math.round(c * 0.8))
-
-        // Set backgrounds / borders
-        songTimelineForegroundEl.style.backgroundColor = `rgb(${scaledColor.join(",")})`
-        songTimelineCircleEl.style.backgroundColor = `rgb(${scaledColor.join(",")})`
-        songTimelineCircleEl.style.borderColor = `rgb(${borderColor.join(",")})`
-    }
 
     // Set end time
     let mp3Length = Math.round((data.play.mods.name.includes("DT") ? data.beatmap.time.mp3Length / 3 * 2 : data.beatmap.time.mp3Length) / 1000)
