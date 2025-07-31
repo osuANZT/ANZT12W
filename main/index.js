fa23a8b0e395784214bc61f2f0436eef995ae5e3
Lawrence Zhou <60532041+illonion@users.noreply.github.com>
2025-07-31 10:32:33 +1000

    Add player names

--- main/index.js
+++ main/index.js
@@ -1,5 +1,19 @@
+// Player names
+const playerNameLeftEl = document.getElementById("player-name-left")
+const playerNameRightEl = document.getElementById("player-name-right")
+let currentPlayerNameLeft, currentPlayerNameRight
+
 const socket = createTosuWsSocket()
 socket.onmessage = event => {
     const data = JSON.parse(event.data)
     console.log(data)
+
+    if (currentPlayerNameLeft !== data.tourney.team.left) {
+        currentPlayerNameLeft = data.tourney.team.left
+        playerNameLeftEl.textContent = currentPlayerNameLeft 
+    }
+    if (currentPlayerNameRight !== data.tourney.team.right) {
+        currentPlayerNameRight = data.tourney.team.right
+        playerNameRightEl.textContent = currentPlayerNameRight 
+    }
 }
