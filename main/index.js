18fec9fd07a56f2a983fba657d5efd02361665bb
Lawrence Zhou <60532041+illonion@users.noreply.github.com>
2025-07-31 10:34:02 +1000

    Add player star container logic

--- main/index.js
+++ main/index.js
@@ -3,11 +3,17 @@ const playerNameLeftEl = document.getElementById("player-name-left")
 const playerNameRightEl = document.getElementById("player-name-right")
 let currentPlayerNameLeft, currentPlayerNameRight
 
+// Player Star Container
+const playerStarContainerLeftEl = document.getElementById("player-star-container-left")
+const playerStarContainerRightEl = document.getElementById("player-star-container-right")
+let currentBestOf, currentFirstTo, currentStarLeft, currentStarRight
+
 const socket = createTosuWsSocket()
 socket.onmessage = event => {
     const data = JSON.parse(event.data)
     console.log(data)
 
+    // Player Names
     if (currentPlayerNameLeft !== data.tourney.team.left) {
         currentPlayerNameLeft = data.tourney.team.left
         playerNameLeftEl.textContent = currentPlayerNameLeft 
@@ -15,5 +21,42 @@ socket.onmessage = event => {
     if (currentPlayerNameRight !== data.tourney.team.right) {
         currentPlayerNameRight = data.tourney.team.right
         playerNameRightEl.textContent = currentPlayerNameRight 
+    }
+
+    // Player Star Container
+    if (currentBestOf !== data.tourney.bestOF ||
+        currentStarLeft !== data.tourney.points.left ||
+        currentStarRight !== data.tourney.points.right
+    ) {
+        currentBestOf = data.tourney.bestOF
+        currentFirstTo = Math.ceil(currentBestOf / 2) 
+        currentStarLeft = data.tourney.points.left
+        currentStarRight = data.tourney.points.right
+
+        playerStarContainerLeftEl.innerHTML = ""
+        playerStarContainerRightEl.innerHTML = ""
+
+        for (let i = 0; i < currentFirstTo; i++) {
+            // Left Stars
+            if (i < currentStarLeft) {
+                playerStarContainerLeftEl.append(createStar("fill"))
+            } else {
+                playerStarContainerLeftEl.append(createStar("empty"))
+            }
+
+            // Right Stars
+            if (i < currentStarRight) {
+                playerStarContainerRightEl.append(createStar("fill"))
+            } else {
+                playerStarContainerRightEl.append(createStar("empty"))
+            }
+        }
+
+        // Create star
+        function createStar(status) {
+            const star = document.createElement("div")
+            star.classList.add("player-star", `player-star-${status}`)
+            return star
+        }
     }
 }
