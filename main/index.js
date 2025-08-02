76b7c84c2e377678c0b82651fd2931842e3d6022
Lawrence Zhou <60532041+illonion@users.noreply.github.com>
2025-08-02 20:24:30 +1000

    Fix stats now being shown properly

--- main/index.js
+++ main/index.js
@@ -226,18 +226,18 @@ socket.onmessage = event => {
             let ar = Number(currentMappoolBeatmap.diff_approach)
             let bpm = Number(currentMappoolBeatmap.bpm)
 
-            if (currentBeatmap.mod.includes("HR")) {
+            if (currentMappoolBeatmap.mod.includes("HR")) {
                 cs = Math.min(Math.round(cs * 1.3 * 10) / 10, 10)
                 ar = Math.min(Math.round(ar * 1.4 * 10) / 10, 10)
             }
-            if (currentBeatmap.mod.includes("DT")) {
+            if (currentMappoolBeatmap.mod.includes("DT")) {
                 if (ar > 5) ar = Math.round((((1200 - (( 1200 - (ar - 5) * 150) * 2 / 3)) / 150) + 5) * 10) / 10
                 else ar = Math.round((1800 - ((1800 - ar * 120) * 2 / 3)) / 120 * 10) / 10
                 bpm = Math.round(bpm * 1.5)
             }
 
             // Set Stats
-            nowPlayingSrEl.textContent = Number(currentMappoolBeatmap.difficultyrating).toFixed(2)
+            nowPlayingSrEl.textContent = `${Number(currentMappoolBeatmap.difficultyrating).toFixed(2)}*`
             nowPlayingBpmEl.textContent = bpm
             nowPlayingCsEl.textContent = cs
             nowPlayingArEl.textContent = ar
